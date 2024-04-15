fbf53f5268e525809dc0d493d9cdce149890f3ea
FatPaper-1874 <360655753@qq.com>
2024-04-15 22:18:15 +0800

    修复admin的问题

--- src/utils/api/user.ts
+++ src/utils/api/user.ts
@@ -1,50 +1,51 @@
 import axios from "axios";
+import {__USERSERVER__} from "../../../global.config"
 
 export const updateUser = async (id: string, username: string, password: string, avatar: string, color: string) => {
-	await axios.post("/user/update", { id, username, password, avatar, color });
+    await axios.post("/user/update", {id, username, password, avatar, color});
 };
 
 export const createUser = async (username: string, password: string, avatar: string, color: string) => {
-	await axios.post("/user/create", { username, password, avatar, color });
+    await axios.post("/user/create", {username, password, avatar, color});
 };
 
 export const deleteUser = async (id: string) => {
-	await axios.delete("/user/delete", { params: { id } });
+    await axios.delete("/user/delete", {params: {id}});
 };
 
 export const getUserList = async (page: number, size: number) => {
-	const { total, userList, current } = (await axios.get("/user/list", { params: { page, size } })) as any;
-	return { total, userList, current };
+    const {total, userList, current} = (await axios.get("/user/list", {params: {page, size}})) as any;
+    return {total, userList, current};
 };
 
 export const getLoginCode = async () => {
-	const res = (await axios.get("/user/get-login-code")) as any;
-	console.log("🚀 ~ file: user.ts:22 ~ getLoginCode ~ res:", res);
+    const res = (await axios.get("/user/get-login-code")) as any;
+    console.log("🚀 ~ file: user.ts:22 ~ getLoginCode ~ res:", res);
 
-	return res as { img: { type: string; data: number[] }; uuid: string };
+    return res as { img: { type: string; data: number[] }; uuid: string };
 };
 
 export const getLoginCodeState = async (uuid: string) => {
-	const res = (await axios.get(`/user/get-code-state?uuid=${uuid}`)) as any;
-	return res as { codeState: number; token?: string };
+    const res = (await axios.get(`/user/get-code-state?uuid=${uuid}`)) as any;
+    return res as { codeState: number; token?: string };
 };
 
 export const isAdmin = async () => {
-	const res = (await axios.get(`/user/is-admin`)) as any;
-	console.log("🚀 ~ file: user.ts:35 ~ isAdmin ~ res:", res)
-	return res as { isAdmin: boolean };
+    const res = (await axios.get(`${__USERSERVER__}/user/is-admin`)) as any;
+    console.log("🚀 ~ file: user.ts:35 ~ isAdmin ~ res:", res)
+    return res as { isAdmin: boolean };
 };
 
 export const checkAdminIdentity = () =>
-	new Promise<boolean>(async (resolve, reject) => {
-		try {
-			const _isAdmin = (await isAdmin()).isAdmin;
-			if (_isAdmin) {
-				resolve(true);
-			} else {
-				reject(false);
-			}
-		} catch (e) {
-			reject(false);
-		}
-	});
+    new Promise<boolean>(async (resolve, reject) => {
+        try {
+            const _isAdmin = (await isAdmin()).isAdmin;
+            if (_isAdmin) {
+                resolve(true);
+            } else {
+                reject(false);
+            }
+        } catch (e) {
+            reject(false);
+        }
+    });
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import ElementPlus from 'unplugin-element-plus/vite';
 import {visualizer} from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        ElementPlus({}),
         visualizer({
             gzipSize: true,
             brotliSize: true,

1a12fb4f708c81d8c6099326aa83e9fbf15b33dd
FatPaper-1874 <360655753@qq.com>
2024-09-29 21:42:58 +0800

    优化fontawesome的引入, 优化打包速度

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {createApp} from "vue";
+import { createApp } from "vue";
 import "@/assets/style.scss";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
@@ -6,33 +6,69 @@ import router from "./router/index";
 import "@/utils/axios";
 
 /* import the fontawesome core */
-import {library} from "@fortawesome/fontawesome-svg-core";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 /* import font awesome icon component */
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import {fas} from "@fortawesome/free-solid-svg-icons";
+import {
+	faBolt,
+	faBomb,
+	faHeart,
+	faHouse,
+	faPalette,
+	faSackDollar,
+	faWandMagicSparkles,
+	faCrown,
+	faMapLocationDot,
+	faBox,
+	faCreditCard,
+	faHeadset,
+	faCircleCheck,
+	faWarning,
+	faPlus,
+	faUpDownLeftRight,
+	faHandPointer,
+} from "@fortawesome/free-solid-svg-icons";
 
 /* add icons to the library */
-library.add(fas);
+library.add(
+	faBolt,
+	faBomb,
+	faHeart,
+	faHouse,
+	faPalette,
+	faSackDollar,
+	faWandMagicSparkles,
+	faCrown,
+	faMapLocationDot,
+	faBox,
+	faCreditCard,
+	faHeadset,
+	faCircleCheck,
+	faWarning,
+	faPlus,
+	faUpDownLeftRight,
+	faHandPointer
+);
 
 const app = createApp(App);
 app.use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
 
 // 修复monaco-editor报错
-import * as monaco from 'monaco-editor';
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+import * as monaco from "monaco-editor";
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 // @ts-ignore
 self.MonacoEnvironment = {
-    getWorker(_: any, label: string) {
-        if (label === 'typescript' || label === 'javascript') {
-            return new tsWorker();
-        }
-        return new editorWorker();
-    }
+	getWorker(_: any, label: string) {
+		if (label === "typescript" || label === "javascript") {
+			return new tsWorker();
+		}
+		return new editorWorker();
+	},
 };
 
 monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);

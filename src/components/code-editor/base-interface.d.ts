74975c4af0ed2acc7e279f05f53bc5961c0c36f1
FatPaper-1874 <360655753@qq.com>
2024-10-20 22:56:15 +0800

    编辑器类型库更新

--- src/components/code-editor/base-interface.d.ts
+++ src/components/code-editor/base-interface.d.ts
@@ -11,37 +11,43 @@ enum GameOverRule {
 	Earn100000, //挣100000块钱
 }
 
-interface GameProcess {
-	gameSetting: GameSetting;
-	isDistory: boolean;
+enum GameLinkItem {
+	Player = "Player",
+	ChanceCard = "ChanceCard",
+	Property = "Property",
+	ArrivedEvent = "ArrivedEvent",
+}
 
-	//Static Data
-	mapId: string;
-	mapName: string;
-	mapBackground: string;
-	propertiesList: Map<string, Property>;
-	mapItemsList: Map<string, MapItem>;
-	chanceCardsList: ChanceCardFromDB[];
-	mapIndexList: string[];
-	playersList: Player[];
-	itemTypesList: ItemType[];
-	streetsList: Street[];
-	animationStepDuration_ms: number;
+interface GameProcess {
+	mapInfo: GameMap;
+	gameSetting: GameSetting;
+	playerList: Player[];
+	propertyList: Map<string, PropertyInterface>;
+	chanceCardInfoList: ChanceCardFromDB[];
+	mapItemList: Map<string, MapItem>;
+	startTime: number;
 
 	//Dynamic Data
-	currentPlayerInRound: Player;
-	currentRound: number;
-	currentMultiplier: number;
-	timeoutList: any[];
-	intervalTimerList: any[];
+	isGameOver: boolean = false;
+	currentPlayerInRound: Player | null = null;
+	currentRound: number = 0; //当前回合
+	currentMultiplier: number = 1; //当前过路费倍数
+	timeoutList: any[]; //计时器列表
+	intervalTimerList: any[]; //计时器列表
+	roundTimeTimer: RoundTimeTimer; //倒计时
+	eventMsg: string = ""; //等待事件的信息
 
-	useChanceCardListener(sourcePlayer: Player): Promise<void>;
-	waitRollDice(player: Player): Promise<void>;
+	//Utils
+	dice: Dice;
+
 	handleArriveEvent(arrivedPlayer: Player): Promise<void>;
-	getRandomChanceCard(num: number): ChanceCard[];
+	getRandomChanceCard(num: number): ChanceCardInterface[];
+	getNewChanceCard(id: string): ChanceCardInterface;
 	gameInfoBroadcast(): void;
 	gameBroadcast(msg: SocketMessage): void;
 	gameMsgNotifyBroadcast(type: "success" | "warning" | "error" | "info", msg: string);
+	gameLogBroadcast(log: string): void;
+	createGameLinkItem(type: GameLinkItem, id: string): string;
 	getPlayerById(id: string): Player | undefined;
 }
 
@@ -121,44 +127,72 @@ interface PlayerEventsCallback {
 	[PlayerEvents.BeforeSetPropertiesList]: (newPropertiesList: PropertyInterface[]) => PropertyInterface[] | undefined;
 	[PlayerEvents.AfterSetPropertiesList]: (newPropertiesList: PropertyInterface[]) => undefined;
 
-	[PlayerEvents.BeforeRound]: (player: PlayerInterface) => Promise<void>;
-	[PlayerEvents.AfterRound]: (player: PlayerInterface) => Promise<void>;
+	[PlayerEvents.BeforeRound]: (
+		player: PlayerInterface
+	) => Promise<PlayerInterface | undefined | void> | PlayerInterface | undefined | void;
+	[PlayerEvents.AfterRound]: (player: PlayerInterface) => Promise<PlayerInterface | undefined | void> | void;
 
-	[PlayerEvents.BeforeGainProperty]: (newProperty: PropertyInterface) => undefined;
-	[PlayerEvents.AfterGainProperty]: (newProperty: PropertyInterface) => undefined;
+	[PlayerEvents.BeforeGainProperty]: (
+		newProperty: PropertyInterface
+	) => Promise<PropertyInterface | undefined | void> | PropertyInterface | undefined | void;
+	[PlayerEvents.AfterGainProperty]: (
+		newProperty: PropertyInterface
+	) => Promise<PropertyInterface | undefined | void> | void;
 
-	[PlayerEvents.BeforeLoseProperty]: (lostProperty: PropertyInterface) => undefined;
-	[PlayerEvents.AfterLoseProperty]: (lostProperty: PropertyInterface) => undefined;
+	[PlayerEvents.BeforeLoseProperty]: (
+		lostProperty: PropertyInterface
+	) => Promise<PropertyInterface | undefined | void> | PropertyInterface | undefined | void;
+	[PlayerEvents.AfterLoseProperty]: (
+		lostProperty: PropertyInterface
+	) => Promise<PropertyInterface | undefined | void> | void;
 
-	[PlayerEvents.BeforeSetCardsList]: (newCardList: ChanceCardInterface[]) => ChanceCardInterface[] | undefined;
-	[PlayerEvents.AfterSetCardsList]: (newCardList: ChanceCardInterface[]) => undefined;
+	[PlayerEvents.BeforeSetCardsList]: (
+		newCardList: ChanceCardInterface[]
+	) => Promise<ChanceCardInterface[] | undefined | void> | ChanceCardInterface[] | undefined | void;
+	[PlayerEvents.AfterSetCardsList]: (
+		newCardList: ChanceCardInterface[]
+	) => Promise<ChanceCardInterface[] | undefined | void> | void;
 
-	[PlayerEvents.BeforeGainCard]: (gainCard: ChanceCardInterface) => ChanceCardInterface | undefined;
-	[PlayerEvents.AfterGainCard]: (gainCard: ChanceCardInterface) => undefined;
+	[PlayerEvents.BeforeGainCard]: (
+		gainCard: ChanceCardInterface
+	) => Promise<ChanceCardInterface | undefined | void> | ChanceCardInterface | undefined | void;
+	[PlayerEvents.AfterGainCard]: (
+		gainCard: ChanceCardInterface
+	) => Promise<ChanceCardInterface | undefined | void> | void;
 
-	[PlayerEvents.BeforeLoseCard]: (lostCard: ChanceCardInterface) => ChanceCardInterface | undefined;
-	[PlayerEvents.AfterLoseCard]: (lostCard: ChanceCardInterface) => undefined;
+	[PlayerEvents.BeforeLoseCard]: (
+		lostCard: ChanceCardInterface
+	) => Promise<ChanceCardInterface | undefined | void> | ChanceCardInterface | undefined | void;
+	[PlayerEvents.AfterLoseCard]: (
+		lostCard: ChanceCardInterface
+	) => Promise<ChanceCardInterface | undefined | void> | void;
 
-	[PlayerEvents.BeforeSetMoney]: (moneyValue: number) => number | undefined;
-	[PlayerEvents.AfterSetMoney]: (moneyValue: number) => undefined;
+	[PlayerEvents.BeforeSetMoney]: (moneyValue: number) => Promise<number | undefined | void> | number | undefined | void;
+	[PlayerEvents.AfterSetMoney]: (moneyValue: number) => Promise<number | undefined | void> | void;
 
-	[PlayerEvents.BeforeGain]: (gainMoney: number, source?: PlayerInterface) => number | undefined;
-	[PlayerEvents.AfterGain]: (gainMoney: number, source?: PlayerInterface) => undefined;
+	[PlayerEvents.BeforeGain]: (
+		gainMoney: number,
+		source?: PlayerInterface
+	) => Promise<number | undefined | void> | number | undefined | void;
+	[PlayerEvents.AfterGain]: (gainMoney: number, source?: PlayerInterface) => Promise<number | undefined | void> | void;
 
-	[PlayerEvents.BeforeCost]: (costMoney: number, target?: PlayerInterface) => number | undefined;
-	[PlayerEvents.AfterCost]: (costMoney: number, target?: PlayerInterface) => undefined;
+	[PlayerEvents.BeforeCost]: (
+		costMoney: number,
+		target?: PlayerInterface
+	) => Promise<number | undefined | void> | number | undefined | void;
+	[PlayerEvents.AfterCost]: (costMoney: number, target?: PlayerInterface) => Promise<number | undefined | void> | void;
 
-	[PlayerEvents.BeforeStop]: (stopValue: number) => number | undefined;
-	[PlayerEvents.AfterStop]: (stopValue: number) => undefined;
+	[PlayerEvents.BeforeStop]: (stopValue: number) => Promise<number | undefined | void> | number | undefined | void;
+	[PlayerEvents.AfterStop]: (stopValue: number) => Promise<number | undefined | void> | void;
 
-	[PlayerEvents.BeforeTp]: (tpValue: number) => number | undefined;
-	[PlayerEvents.AfterTp]: (tpValue: number) => undefined;
+	[PlayerEvents.BeforeTp]: (tpValue: number) => Promise<number | undefined | void> | number | undefined | void;
+	[PlayerEvents.AfterTp]: (tpValue: number) => Promise<number | undefined | void> | void;
 
-	[PlayerEvents.BeforeWalk]: (walkValue: number) => number | undefined;
-	[PlayerEvents.AfterWalk]: (walkValue: number) => undefined;
+	[PlayerEvents.BeforeWalk]: (walkValue: number) => Promise<number | undefined | void> | number | undefined | void;
+	[PlayerEvents.AfterWalk]: (walkValue: number) => Promise<number | undefined | void> | void;
 
-	[PlayerEvents.BeforeSetBankrupted]: (isBankrupted: boolean) => boolean;
-	[PlayerEvents.AfterSetBankrupted]: (isBankrupted: boolean) => undefined;
+	[PlayerEvents.BeforeSetBankrupted]: (isBankrupted: boolean) => Promise<boolean> | boolean;
+	[PlayerEvents.AfterSetBankrupted]: (isBankrupted: boolean) => Promise<boolean | undefined | void> | void;
 }
 
 interface PropertyInterface {
@@ -175,13 +209,16 @@ interface PropertyInterface {
 	getPassCost: () => number;
 
 	//设置房产信息
-	setOwner: (player: PlayerInterface | undefined) => void;
+	setOwner: (player: PlayerInterface | undefined) => Promise<void>;
 	setBuildingLevel: (level: 0 | 1 | 2) => void;
 
 	getPropertyInfo: () => PropertyInfo;
 }
 
 interface PlayerInterface {
+	//自定义参数
+	extras: Record<string, any>;
+
 	//玩家信息
 	getUser: () => User;
 	getId: () => string;
@@ -190,8 +227,8 @@ interface PlayerInterface {
 	//地产相关
 	getPropertiesList: () => PropertyInterface[];
 	setPropertiesList: (newPropertiesList: PropertyInterface[]) => void;
-	gainProperty: (property: PropertyInterface) => void;
-	loseProperty: (property: PropertyInterface) => void;
+	gainProperty: (property: PropertyInterface) => Promise<void>;
+	loseProperty: (property: PropertyInterface) => Promise<void>;
 
 	//机会卡相关
 	getCardsList: () => ChanceCardInterface[];
@@ -231,6 +268,7 @@ interface PlayerInterface {
 
 interface ChanceCardInterface {
 	getId: () => string;
+	getSourceId: () => string;
 	getName: () => string;
 	getDescribe: () => string;
 	getIcon: () => string;
@@ -243,7 +281,23 @@ interface ChanceCardInterface {
 		gameProcess: GameProcess
 	) => Promise<void>;
 
-	getChanceCardInfo: () => ChanceCardInfo;
+	getChanceCardInfo: () => ChanceCardInstanceInfo;
+}
+
+interface GameMap {
+	id: string;
+	name: string;
+	background: string;
+	mapItems: MapItem[];
+	properties: Property[];
+	chanceCards: ChanceCard[];
+	itemTypes: ItemType[];
+	indexList: string[];
+	streets: Street[];
+	inUse: boolean;
+	houseModel_lv0: Model;
+	houseModel_lv1: Model;
+	houseModel_lv2: Model;
 }
 
 interface ChanceCardFromDB {
@@ -254,6 +308,16 @@ interface ChanceCardFromDB {
 	color: string;
 	type: ChanceCardType;
 	effectCode: string;
+}
+
+interface ChanceCardInstanceInfo {
+	id: string;
+	sourceId: string;
+	name: string;
+	describe: string;
+	color: string;
+	type: ChanceCardType;
+	icon: string;
 }
 
 interface Street {
@@ -314,4 +378,5 @@ interface Buff {
 
 type Utils = {
 	randomString: (length: number) => string;
+	randomInRange: (min: number, max: number) => number;
 };
